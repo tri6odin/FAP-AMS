65f62db199e03d01ba7d35c568c7bf3f2950566b
tri6odin <tri6odin@gmail.com>
2023-11-19 15:24:10 +0300

    Migration to pydv2

--- database/schemas/profile.py
+++ database/schemas/profile.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel, EmailStr, conint, Field, validator
+from pydantic import BaseModel, EmailStr, Field, field_validator
 from typing import Optional
 
 from config import NICKNAME_MAX_LENGTH, NICKNAME_MIN_LENGTH, STR_MAX_LENGTH
+from typing_extensions import Annotated
 
 
 class requestProfileSchema(BaseModel):
@@ -11,7 +12,7 @@ class requestProfileSchema(BaseModel):
         None, min_length=1, max_length=STR_MAX_LENGTH)
     nickname: Optional[str] = Field(
         None, min_length=NICKNAME_MIN_LENGTH, max_length=NICKNAME_MAX_LENGTH)
-    age: Optional[conint(ge=18)] = None
+    age: Optional[Annotated[int, Field(ge=18)]] = None
 
 
 class responseProfileSchema(BaseModel):
@@ -23,9 +24,11 @@ class responseProfileSchema(BaseModel):
         None, min_length=1, max_length=STR_MAX_LENGTH)
     nickname: Optional[str] = Field(
         None, min_length=NICKNAME_MIN_LENGTH, max_length=NICKNAME_MAX_LENGTH)
-    age: Optional[conint(ge=18)] = None
-    password: bool = False
+    age: Optional[Annotated[int, Field(ge=18)]] = None
+    password: Optional[str] = None
 
-    @validator('password', pre=True, always=True)
+    @field_validator('password')
     def hash_password(cls, v):
+        if v is None:
+            return False
         return bool(v)
